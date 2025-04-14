7419dfb41db8094c6071adfd8c077df75a6ef41f
Rina-ui <marinagracia895@gmail.com>
2025-04-14 08:52:48 +0200

    modification

--- backend/app.js
+++ backend/app.js
@@ -22,17 +22,34 @@ const app = express();
 //     console.log('Réponse envoyée avec succès !');
 // })
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json()); // Middleware pour analyser le corps des requêtes JSON
+
 
 // Middleware CORS 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet cree !'
   });
+
+});
   
   // Route API
-  app.use('/api/stuff', (req, res, next) => {
+  app.get('/api/stuff', (req, res, next) => {
     const stuff = [
       {
         _id: 'oejfoejf',
